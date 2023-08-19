a037bcaedb1a36efa9ac7f278d6bb64100cd768b
skyfroger <skyfroger@gmail.com>
2023-08-19 14:32:18 +0300

    feat: запуск робота по команде - метод wait()

--- src/RPulse.cpp
+++ src/RPulse.cpp
@@ -1,2 +1,56 @@
 #include "Arduino.h"
+#include "GyverTimers.h"
 #include "RPulse.h"
+
+RPulse::RPulse()
+{
+}
+
+/**
+ * Инициализация соединения
+ */
+void RPulse::init(int baudRate)
+{
+    Serial.begin(baudRate);
+}
+
+/**
+ * Ожидание команды для запуска робота
+ */
+void RPulse::wait()
+{
+    String command;
+
+    while (true)
+    {
+        char message[10];
+
+        // читаем байты до символа ;
+        int numBytes = Serial.readBytesUntil(';', message, 10);
+        // если есть прочитанные байты...
+        if (numBytes > 0)
+        {
+            // ... преобразуем их в строку
+            command = charArrayToString(message, numBytes);
+            if (command == COMMAND_START)
+            {
+                // пришла подходящая команда
+                break;
+            }
+        }
+    }
+}
+
+/**
+ * Преобразование массива символов в строку
+ */
+String RPulse::charArrayToString(char *a, int size)
+{
+    int i;
+    String s = "";
+    for (i = 0; i < size; i++)
+    {
+        s = s + a[i];
+    }
+    return s;
+}
--- src/RPulse.h
+++ src/RPulse.h
@@ -2,7 +2,19 @@
 #define RPulse_h
 
 #include "Arduino.h"
+#include "GyverTimers.h"
 
+#define COMMAND_START "p>start" // команда для запуска эксперимента
 
+class RPulse
+{
+public:
+    RPulse();                // конструктор
+    void init(int baudRate); // открываем последовательный порт
+    void wait();             // ждём получения сообщения P>start;
+
+private:
+    static String charArrayToString(char *a, int size);
+};
 
 #endif
